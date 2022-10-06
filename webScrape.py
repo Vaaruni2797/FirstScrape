f69af423c6b971beb734476c3a0435fbbd2d52aa
Vaaruni <vaaruni.desai@gmail.com>
2022-10-05 21:26:28 -0700

    File Update

--- webScrape.py
+++ webScrape.py
@@ -1,11 +1,86 @@
-import config
 import tweepy
 from tweepy import OAuthHandler
-from config import consumer_key,consumer_secret,access_secret,access_token
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
- 
-api = tweepy.API(auth)
-for status in tweepy.Cursor(api.home_timeline).items(10):
-    # Process a single status
-    print(status.text)
+from tweepy import Stream
+from tweepy.streaming import StreamingClient
+import time
+import argparse
+import string
+import config
+import json
+
+def get_parser():
+    """Get parser for command line arguments."""
+    parser = argparse.ArgumentParser(description="Twitter Downloader")
+    parser.add_argument("-q",
+                        "--query",
+                        dest="query",
+                        help="Query/Filter",
+                        default='-')
+    parser.add_argument("-d",
+                        "--data-dir",
+                        dest="data_dir",
+                        help="Output/Data Directory")
+    return parser
+
+
+class MyListener(StreamingClient):
+    """Custom StreamListener for streaming data."""
+
+    def __init__(self, data_dir, query):
+        query_fname = format_filename(query)
+        self.outfile = "%s/stream_%s.json" % (data_dir, query_fname)
+
+    def on_data(self, data):
+        try:
+            with open(self.outfile, 'a') as f:
+                f.write(data)
+                print(data)
+                return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+            time.sleep(5)
+        return True
+
+    def on_error(self, status):
+        print(status)
+        return True
+
+
+def format_filename(fname):
+    """Convert file name into a safe string.
+    Arguments:
+        fname -- the file name to convert
+    Return:
+        String -- converted file name
+    """
+    return ''.join(convert_valid(one_char) for one_char in fname)
+
+
+def convert_valid(one_char):
+    """Convert a character into '_' if invalid.
+    Arguments:
+        one_char -- the char to convert
+    Return:
+        Character -- converted char
+    """
+    valid_chars = "-_.%s%s" % (string.ascii_letters, string.digits)
+    if one_char in valid_chars:
+        return one_char
+    else:
+        return '_'
+
+@classmethod
+def parse(cls, api, raw):
+    status = cls.first_parse(api, raw)
+    setattr(status, 'json', json.dumps(raw))
+    return status
+
+if __name__ == '__main__':
+    parser = get_parser()
+    args = parser.parse_args()
+    auth = OAuthHandler(config.consumer_key, config.consumer_secret, config.access_token, config.access_token_secret)
+    auth.set_access_token(config.access_token, config.access_token_secret)
+    api = tweepy.API(auth)
+
+    twitter_stream = Stream(auth, MyListener(args.data_dir, args.query))
+    twitter_stream.filter(track=[args.query])
